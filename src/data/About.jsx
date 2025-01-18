ecced163b2baea462590b9fdbf9bf6e39d2b7970
rohit204k <gundakallirohit@gmail.com>
2025-01-17 20:34:12 -0600

    Added react files

---
+++ src/data/About.jsx
@@ -0,0 +1,10 @@
+export const aboutData = [
+    {
+      description: [
+        "Hey there 👋!",
+        "I’m Rohit Kadappa Gundakalli, a recent graduate with a Master’s in Computer Science from the University of Massachusetts Amherst. Throughout my academic journey, I developed a solid foundation in core areas such as Data Structures, Algorithms, Machine Learning, and Cloud Systems. I've also gained practical experience in software development, working on innovative projects and enhancing my technical skills at Wow Labz Pvt. Ltd. and Ellucian.",
+        "During my time at UMass, I specialized in advanced topics like Natural Language Processing, Reinforcement Learning, and Distributed Systems. I’m passionate about leveraging cutting-edge technologies, including Python, Kafka, and AWS, to drive impactful solutions.", 
+        "I’m eager to contribute to the tech industry and collaborate on projects that push the boundaries of innovation. Let’s connect and explore how we can work together to shape the future of technology!"
+      ]
+    }
+  ];
